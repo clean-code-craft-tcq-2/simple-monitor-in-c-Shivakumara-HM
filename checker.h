cb4a609261b9e4f0bf90e2051652f0cba94f9cf9
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-02-09 00:00:09 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -3,3 +3,8 @@
 #define MIN_THRESHOLD_BATT_SoC 20
 #define MAX_THRESHOLD_BATT_SoC 80
 #define MAX_THRESHOLD_BATT_CHARGE_RATE 0.8
+
+int IsTemperatureOK(float temperature);
+int IsSocOK(float soc);
+int IsChargRateOK(float chargeRate);
+int batteryIsOk(float tempertaure, float soc, float chargerate, int (*fpIsTemperatureOK)(float),int (*fpIsSocOK)(float),int (*fpIsChargRateOK)(float));
