3b7888177621ebabff91f2f649e0bccc4bdd213f
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-02-16 09:04:43 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -3,6 +3,11 @@
 #define MIN_THRESHOLD_BATT_SoC 20
 #define MAX_THRESHOLD_BATT_SoC 80
 #define MAX_THRESHOLD_BATT_CHARGE_RATE 0.8
+#define ENGLISH 0
+#define GERMAN 1
+
+enum alertLanguageParameter[[ENGLISH]= {NORMAL, LOW_BREACH, LOW_WARNING, HIGH_WARNING, HIGH_BREACH}, 
+                            [GERMAN] = {Normale, Niedrige Verletzung, Niedrige Warnung, Hohe Warnung, Hoher Bruch}]
 
 int IsTemperatureOK(float temperature);
 int IsSocOK(float soc);
