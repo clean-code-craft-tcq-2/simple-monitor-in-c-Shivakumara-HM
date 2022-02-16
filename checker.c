180f810abd647fd54b23d1c26cddac08b8b9296d
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-02-16 19:39:40 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -129,5 +129,5 @@ int batteryIsOk(float tempertaure, float soc, float chargerate, int lowLimit, in
 int main() 
 {
   assert(batteryIsOk(25, 70, 0.7, MIN_THRESHOLD, MAX_THRESHOLD, ENGLISH, 0));
-  assert(!batteryIsOk(50, 85, 0, MIN_THRESHOLD, MAX_THRESHOLD, GERMAN, 0));
+  assert(!batteryIsOk(50, 85, 0, MIN_THRESHOLD, MAX_THRESHOLD, GERMAN, 1));
 }
