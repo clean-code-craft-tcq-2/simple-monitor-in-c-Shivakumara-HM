473aa8620a61834fba20bca64642f49c1eeacd18
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-02-22 23:56:25 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -39,7 +39,7 @@ int checkHigherThreshold(float input, float highLimit, int language)
 int ConvertFarenheitToCelcius(float farenheit)
 {
   float temperature = ((farenheit - 32) * 5 / 9);
-  return(int TempStatus = IsTemperatureOK(temperature, lowLimit, highLimit, language);)
+  return(IsTemperatureOK(temperature, lowLimit, highLimit, language));
 }
 
 /*  pure function to check Temperature range */
@@ -127,7 +127,7 @@ int batteryIsOk(float temperature, char unit, float soc, float chargerate, int l
 {
   if (temperature == 'F')
   {
-  int TempStatus = ConvertFarenheitToCelcius(tempertaure);
+  int TempStatus = ConvertFarenheitToCelcius(temperature);
   }
   else
   {
